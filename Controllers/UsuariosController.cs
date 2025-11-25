3380a2a5c8d76092c075e240eba7a5397ed878fd
diego.faoliveira <diego.faoliveira@TIT0676454W11-1>
2025-11-25 20:21:45 -0300

    Adicionar arquivos de projeto.

---
+++ Controllers/UsuariosController.cs
@@ -0,0 +1,104 @@
+﻿using apiAutenticacao.Data;
+using apiAutenticacao.Models;
+using apiAutenticacao.Models.DTO;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using static BCrypt.Net.BCrypt;
+
+namespace apiAutenticacao.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsuariosController : ControllerBase
+    {
+        private readonly AppDbContext _context;
+
+        public UsuariosController(AppDbContext context) { 
+        
+            _context = context;
+
+        }
+
+       [HttpPost("cadastrar")]
+       public async Task<IActionResult> CadastrarUsuarioAsync([FromBody] CadastroUsuarioDTO dadosUsuario) {
+
+            if (!ModelState.IsValid) { 
+
+                return BadRequest(ModelState);
+            }
+
+            Usuario? usuarioExistente = await _context.Usuarios.
+                FirstOrDefaultAsync(usuario => usuario.Email == dadosUsuario.Email);
+
+            if (usuarioExistente != null) {
+
+                return BadRequest(new { erro = true, mensagem = "Este email já está cadastrado" });
+            
+            }
+
+            Usuario usuario = new Usuario { 
+            
+                Nome = dadosUsuario.Nome,
+                Email = dadosUsuario.Email,
+                Senha = HashPassword(dadosUsuario.Senha),
+                ConfirmarSenha = HashPassword(dadosUsuario.ConfirmarSenha)
+                
+
+            };
+
+            _context.Usuarios.Add(usuario);
+            await _context.SaveChangesAsync();
+
+            return Ok(new { 
+
+                erro = false,
+                mensagem = "Usuário criado com sucesso",
+                usuario = new {
+
+                    id = usuario.Id,
+                    nome = usuario.Nome,
+                    email = usuario.Email
+
+                }
+               
+            
+            });
+
+        
+        
+        }
+
+       [HttpPost("login")]
+       public async Task<IActionResult> Login([FromBody] LoginDTO dadosUsuario) {
+
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            Usuario? usuarioEncontrado = await _context.Usuarios.
+                FirstOrDefaultAsync(usuario => usuario.Email == dadosUsuario.Email);
+
+            if (usuarioEncontrado != null)
+            {
+                bool isValidPassword = Verify(dadosUsuario.Senha, usuarioEncontrado.Senha);
+
+                if (isValidPassword)
+                {
+                    return Ok("Login realizado com sucesso");
+                    
+                }
+
+                return Unauthorized("Login não realizado. Email ou senha incorretos");
+
+            }
+
+            return NotFound("Usuário não encontrado!");
+
+
+        }
+
+
+    }
+}
